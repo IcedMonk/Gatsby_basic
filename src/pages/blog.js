c280d373587c0f73b454b46367ae7963d92d153e
IcedMonk <sonawanesamyek@gmail.com>
2019-06-10 01:15:01 +0530

    worked with variables in graphql, and made the blog work with the slug with blog template or we say show page for particular blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,18 +15,24 @@ const blogPage = () => {
               title
               Date
             }
+            fields {
+              slug
+            }
           }
         }
       }
     }
   `)
+  console.log(data);
   return (
     <Layout>
       <h1>Blog</h1>
       <ol>
           {data.allMarkdownRemark.edges.map((edge) => (<li>
-            <h2>{edge.node.frontmatter.title}</h2>
-            <p>{edge.node.frontmatter.Date}</p>
+            <Link to={`/blog/${edge.node.fields.slug}`}>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.Date}</p>
+            </Link>
           </li>))}
       </ol>
     </Layout>
